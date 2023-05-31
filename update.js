d5a76c121e882c0c81b8f4a32ca43b653de384a5
numens <349690177@qq.com>
2023-05-31 11:16:24 +0800

    Update update.js

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 function update() {
     const data = JSON.stringify(new Date());
-    fs.writeFile('README.md', data, (err) => {
+    fs.writeFile('./README.md', data, (err) => {
         if (err) console.log('error');
         else console.log('success');
     });
